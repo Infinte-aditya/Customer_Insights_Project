64b77935d12ece238c29e6b747f7adc5296d4cf5
Infinte-aditya <apwb.musgrave@gmail.com>
2025-02-19 18:12:47 +0530

    major changes

--- backend/src/routes/feedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createFeedback, getAllFeedback } = require("../controllers/feedbackController");
-const router = express.Router();
-
-router.post("/feedback", createFeedback);
-router.get("/feedback", getAllFeedback);
-
-module.exports = router;
