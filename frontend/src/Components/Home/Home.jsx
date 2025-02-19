393d7b07f17f89adc7b7e3d0b1454bd9939c1c52
Infinte-aditya <apwb.musgrave@gmail.com>
2025-02-19 11:23:09 +0530

    body creation for the home page

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Home.css'
+import pic from '../../Assets/pic.jpg';
 
 const Home = () => {
   return (
-    <div>
+    <div className='container'>
+      <div className='body'>
+        <div className='pic'>
+          <img src={pic} alt='body pic'/>
+        </div>
+        <div className='intro'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Et deleniti provident vel consequuntur, nisi doloribus, quisquam 
+            consequatur veniam, unde asperiores quibusdam ducimus magni? Ratione 
+            sint autem odit nesciunt, fugiat nemo!</p>
+        </div>
+
+      </div>
       
     </div>
   )
